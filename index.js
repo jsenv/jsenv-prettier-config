5772d4779b2b9dc0ae638683c65252fbdb0fd446
Damien Maillard <damien.maillard@dailymotion.com>
2018-09-25 15:47:21 +0200

    v1.0.0

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  semi: false,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: "all",
+  // arrow function with a single parameter is conceptually great
+  // BUT
+  // param destructuring requires parenthesis
+  // when adding new parameter it becomes a syntax error you have to manually
+  // add parenthesis back, which is painful
+  arrowParens: "always",
+}
