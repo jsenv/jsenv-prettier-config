36d8328b02d3f17b288b41d52f3643c7e6c9c9b7
Damien Maillard <damien.maillard@dailymotion.com>
2020-01-21 15:54:20 +0100

    1.2.0
    - add .js file for node>=13
    - add .cjs file for node<13

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-module.exports = {
-  printWidth: 100,
-  semi: false,
-  tabWidth: 2,
-  useTabs: false,
-  trailingComma: "all",
-  quoteProps: "consistent",
-  // arrow function with a single parameter is conceptually great
-  // BUT
-  // param destructuring requires parenthesis
-  // when adding new parameter it becomes a syntax error you have to manually
-  // add parenthesis back, which is painful
-  arrowParens: "always",
-}
+import { createRequire } from "module"
+
+const require = createRequire(import.meta.url)
+const prettierConfig = require("./index.cjs")
+
+export default prettierConfig
